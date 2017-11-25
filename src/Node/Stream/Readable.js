130ffcbde27197d8aee880eed20d0b5dc2fb9a47
Matthew Leon <ml@matthewleon.com>
2017-11-24 22:40:04 -0500

    necessary functions for custom Readable

--- src/Node/Stream/Readable.js
+++ src/Node/Stream/Readable.js
@@ -5,8 +5,22 @@ var stream = require("stream");
 exports.mkReadable = function (readCb) {
   var stream = new stream.Readable({
     read: function(size) {
-      readCb(stream, size);
+      readCb(stream.push, size);
     }
   });
   return stream;
 };
+
+function push(pushFn) {
+  return pushFn;
+};
+
+exports.pushBuffer = pushFn;
+exports.pushUint8Array = pushFn;
+exports.pushString = pushFn;
+exports.pushEnd = function (pushFn) {
+  pushFn(null);
+};
+
+// TODO
+// exports.pushStringWithEncoding;
